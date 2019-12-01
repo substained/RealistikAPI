981800ef3ac0cc2860b63a427361a938fe5b02ef
Kanaze <33534507+Kanaze-chan@users.noreply.github.com>
2019-12-01 23:48:03 +0700

    Sunhapan is tired.

--- app/start.go
+++ app/start.go
@@ -94,6 +94,7 @@ func Start(conf common.Conf, dbO *sqlx.DB) *fhr.Router {
 		r.Method("/api/v1/users", v1.UsersGET)
 		r.Method("/api/v1/users/whatid", v1.UserWhatsTheIDGET)
 		r.Method("/api/v1/users/full", v1.UserFullGET)
+		r.Method("/api/v1/users/rxfull", v1.RelaxUserFullGET)
 		r.Method("/api/v1/users/achievements", v1.UserAchievementsGET)
 		r.Method("/api/v1/users/most_played", v1.UserMostPlayedGET)
 		r.Method("/api/v1/users/userpage", v1.UserUserpageGET)
