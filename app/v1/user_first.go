abbee906032d6741624b79bac96c8dce1a42a666
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2020-04-27 23:14:57 +0200

    tactical c+p (ap first scores)

--- app/v1/user_first.go
+++ app/v1/user_first.go
@@ -95,6 +95,36 @@ func UserFirstGET(md common.MethodData) common.CodeMessager {
 		
 		r.Scores = append(r.Scores, nc)
 	}
+
+	// Fetch all scores from users
+	rows, err = md.DB.Query("SELECT scores_ap.id, scores_ap.beatmap_md5, scores_ap.score, scores_ap.max_combo, scores_ap.full_combo, scores_ap.mods, scores_ap.300_count, scores_ap.100_count, scores_ap.50_count, scores_ap.katus_count, scores_ap.gekis_count, scores_ap.misses_count, scores_ap.time, scores_ap.play_mode, scores_ap.accuracy, scores_ap.pp, scores_ap.completed, beatmaps.beatmap_id, beatmaps.beatmapset_id, beatmaps.beatmap_md5, beatmaps.song_name, beatmaps.ar, beatmaps.od, beatmaps.difficulty_std, beatmaps.difficulty_std, beatmaps.difficulty_taiko, beatmaps.difficulty_ctb, beatmaps.difficulty_mania, beatmaps.max_combo, beatmaps.hit_length, beatmaps.ranked, beatmaps.ranked_status_freezed, beatmaps.latest_update FROM scores_first, scores_ap, beatmaps WHERE scores_first.scoreid=scores_ap.id AND scores_ap.beatmap_md5=beatmaps.beatmap_md5 AND scores_first.userid = ? AND scores_ap.play_mode = ? " + common.Paginate(md.Query("p"), md.Query("l"), 50), id, mode)
+	if err != nil {
+		md.Err(err)
+		return Err500
+	}
+	defer rows.Close()
+	for rows.Next() {
+		nc := userScore{}
+		err = rows.Scan(&nc.Score.ID, &nc.Score.BeatmapMD5, &nc.Score.Score, &nc.Score.MaxCombo, &nc.Score.FullCombo, &nc.Score.Mods, &nc.Score.Count300, &nc.Score.Count100, &nc.Score.Count50, &nc.Score.CountKatu, &nc.Score.CountGeki, &nc.Score.CountMiss, &nc.Score.Time, &nc.Score.PlayMode, &nc.Score.Accuracy, &nc.Score.PP, &nc.Score.Completed, &nc.Beatmap.BeatmapID, &nc.Beatmap.BeatmapsetID, &nc.Beatmap.BeatmapMD5, &nc.Beatmap.SongName, &nc.Beatmap.AR, &nc.Beatmap.OD, &nc.Beatmap.Difficulty, &nc.Beatmap.Diff2.STD, &nc.Beatmap.Diff2.Taiko, &nc.Beatmap.Diff2.CTB, &nc.Beatmap.Diff2.Mania, &nc.Beatmap.MaxCombo, &nc.Beatmap.HitLength, &nc.Beatmap.Ranked, &nc.Beatmap.RankedStatusFrozen, &nc.Beatmap.LatestUpdate)
+		if err != nil {
+			md.Err(err)
+		}
+		nc.Rank = strings.ToUpper(getrank.GetRank(
+			osuapi.Mode(nc.PlayMode),
+			osuapi.Mods(nc.Mods),
+			nc.Accuracy,
+			nc.Count300,
+			nc.Count100,
+			nc.Count50,
+			nc.CountMiss,
+		))
+		
+		if err != nil {
+			md.Err(err)
+		}
+		
+		r.Scores = append(r.Scores, nc)
+	}
 	
 	r.ResponseBase.Code = 200
 	return r
